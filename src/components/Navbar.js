18d772167ed5ed0fd62a2490e620f512930b3951
Milena Souto <yamadasouto@gmail.com>
2022-12-13 11:06:15 -0800

    add proptype to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
-function Navbar(){
-    const links=["Home", "Add Bike", "Edit Bike"];
-    return(
-        <nav>{links.map((link) => <a href="http://www.adadevelopersacademy.org">{link}</a>)}</nav>
-    )
-
+function Navbar() {
+	const links = ["Home", "Add Bike", "Edit Bike"];
+	return (
+		<nav>
+			{links.map((link) => (
+				<a key={link} href="http://www.adadevelopersacademy.org">
+					{link}
+				</a>
+			))}
+		</nav>
+	);
 }
 
 export default Navbar;
