1def0c88edaa3f2064e282944881ac2023b754f1
Milena Souto <yamadasouto@gmail.com>
2022-12-13 11:04:20 -0800

    Move bikes array to App and pass array into bikesList component as a prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import BikeList from './components/BikeList.js';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import BikeList from "./components/BikeList.js";
+
+const bikesList = [
+	{
+		id: 1,
+		name: "This is some data that we will work to display next class!",
+		size: 50,
+		price: 100,
+		type: "special",
+	},
+	{
+		id: 2,
+		name: "This is some data that we will work to display next class!",
+		size: 40,
+		price: 20,
+		type: "duplicate",
+	},
+	{
+		id: 3,
+		name: "New Bike",
+		size: 80,
+		price: 100,
+		type: "unique",
+	},
+];
 
 function App() {
-  return (
-    <div>
-        <Navbar/>
-        <BikeList/>
-        <button>Add Bike</button>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+
+			{/* send bikesList to bikesList component as a prop */}
+			<BikeList bikesList={bikesList} />
+
+			<button>Add Bike</button>
+		</div>
+	);
 }
 
 export default App;
