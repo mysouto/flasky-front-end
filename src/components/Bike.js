f2a34aa91bf42073b331b734adae5f6df71048ad
Milena Souto <yamadasouto@gmail.com>
2022-12-13 11:12:34 -0800

    Add props to components and generate item components iwth a loop

--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,26 +1,30 @@
+import PropTypes from "prop-types";
+
 import "./Bike.css";
 
-function Bike(){
-    const myBike = {
-        id: 1,
-        name: "My bike to be rendered",
-        size: 50,
-        price: 100,
-        type: "special"
-    };
+function Bike(props) {
+	// craeting variables for each Bike attribute
+	const bikeName = props.name;
+	const bikeId = props.id;
+	const bikeSize = props.size;
+	const bikeType = props.type;
+	const bikePrice = props.price;
 
-    return(
-        <div>
-            <h2 className="bike__name">{myBike.name}</h2>
-            <ul>
-                <li>{myBike.id}</li>
-                <li>{myBike.size}</li>
-                <li>{myBike.price}</li>
-                <li>{myBike.type}</li>
-            </ul>
-            
-        </div>
-    );
+	return (
+		<div>
+			<h2 className="bike__name">{bikeName}</h2>
+			<ul>
+				<li>{bikeId}</li>
+				<li>{bikeSize}</li>
+				<li>{bikeType}</li>
+				<li>{bikePrice}</li>
+			</ul>
+		</div>
+	);
 }
+
+Bike.propTypes = {
+	id: PropTypes.number.isRequired,
+};
 
 export default Bike;
--- src/components/BikeList.js
+++ src/components/BikeList.js
@@ -1,24 +1,43 @@
+// organizing imports, put external libraries at the top
+import PropTypes from "prop-types";
+
 import Bike from "./Bike";
 
-function BikeList() {
-	const bikesList = [
-		{
-			id: 1,
-			name: "This is some data that we will work to display next class!",
-			size: 50,
-			price: 100,
-			type: "special",
-		},
-		{
-			id: 2,
-			name: "This is some data that we will work to display next class!",
-			size: 40,
-			price: 20,
-			type: "duplicate",
-		},
-	];
+// app is sending props to BikeList
+function BikeList({ bikesList }) {
+	const bikeComponents = [];
+	for (const bike of bikesList) {
+		bikeComponents.push(
+			<li key={bike.id}>
+				<Bike
+					id={bike.id}
+					name={bike.name}
+					size={bike.size}
+					price={bike.price}
+					type={bike.type}
+				/>
+			</li>
+		);
+	}
 
-	return bikesList.map((bike) => <Bike />);
+	// using data from bikeList to create new components
+	// return bikesList.map((bike) => (
+	return <div>{bikeComponents};</div>;
 	//returns [<Bike/>, <Bike/>]
 }
+
+// create prop types
+BikeList.propTypes = {
+	// bikesList: PropTypes.array.isRequired,
+	bikesList: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			name: PropTypes.string.isRequired,
+			size: PropTypes.number.isRequired,
+			type: PropTypes.string.isRequired,
+			price: PropTypes.number.isRequired,
+		})
+	),
+};
+
 export default BikeList;
