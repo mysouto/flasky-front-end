6cf3a765202519c53fa180247d5b69519ab4d3c4
Milena Souto <yamadasouto@gmail.com>
2022-12-16 11:56:33 -0800

    Update App component to add axios, API and useEffect

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import Navbar from "./components/Navbar";
 import BikeList from "./components/BikeList.js";
+import axios from "axios";
 
 // data will typically come from an API
 // hardcoded below
-const INITIAL_BIKES = [
-	{
-		id: 1,
-		name: "Cool",
-		size: 50,
-		price: 100,
-		type: "special",
-	},
-	{
-		id: 2,
-		name: "Nice",
-		size: 40,
-		price: 20,
-		type: "duplicate",
-	},
-	{
-		id: 3,
-		name: "New Bike",
-		size: 80,
-		price: 100,
-		type: "unique",
-	},
-];
+// const INITIAL_BIKES = [
+// 	{
+// 		id: 1,
+// 		name: "Cool",
+// 		size: 50,
+// 		price: 100,
+// 		type: "special",
+// 	},
+// 	{
+// 		id: 2,
+// 		name: "Nice",
+// 		size: 40,
+// 		price: 20,
+// 		type: "duplicate",
+// 	},
+// 	{
+// 		id: 3,
+// 		name: "New Bike",
+// 		size: 80,
+// 		price: 100,
+// 		type: "unique",
+// 	},
+// ];
 
 function App() {
+	const [bikesList, setBikesList] = useState([]);
+
+	// API lesson
+	// -- add path to API
+	const URL = "http://127.0.0.1:5000/bike";
+	// const URL = "http://localhost:5000/bike";
+
+	// -- add useEffect
+	useEffect(() => {
+		axios
+			.get(URL)
+			.then((response) => {
+				// console.log(response);
+				const bikesAPIResCopy = response.data.map((bike) => {
+					return {
+						id: bike.id,
+						name: bike.name,
+						price: bike.price,
+						size: bike.size,
+						type: bike.type,
+					};
+				});
+				setBikesList(bikesAPIResCopy);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
 	// 1. make a deep copy of initial data
-	const initialCopy = INITIAL_BIKES.map((bike) => {
-		return { ...bike };
-	});
+	// const initialCopy = INITIAL_BIKES.map((bike) => {
+	// 	return { ...bike };
+	// });
 
 	// WHERE DATA NEEDS TO CHANGE: bikesList
 	// 2. create state
-	const [bikesList, setBikesList] = useState(initialCopy);
+	// const [bikesList, setBikesList] = useState(initialCopy);
 
 	// 3. define callback functions to move logic here
 
@@ -51,39 +81,59 @@ function App() {
 		console.log("updatePrice called");
 
 		const newBikesList = [];
-		for (const bike of bikesList) {
-			// condition: doens't match input bikeId
-			if (bike.id !== bikeId) {
-				// add bike without changing any properties of original bike component
-				newBikesList.push(bike);
-			} else {
-				// if match found, update Price property and keep all other properties the same
-				const newBike = {
-					// ... spread operator keeps all properties the same, copies things as they were
-					...bike,
-					// changing the price here
-					price: newPrice,
-				};
-				newBikesList.push(newBike);
-			}
-		}
 
-		// change bikesList with setBikesList
-		// change state with new list
-		// NEED TO USE SET FUNCTION AND USE NEW ARRAY TO UPDATE STATE TO USE NEW LIST
-		setBikesList(newBikesList);
+		// BACKEND - flask server
+		// add URL made in backend PATCH route definition
+		axios
+			.patch(`${URL}/${bikeId}/${newPrice}`)
+			.then(() => {
+				for (const bike of bikesList) {
+					// condition: doens't match input bikeId
+					if (bike.id !== bikeId) {
+						// add bike without changing any properties of original bike component
+						newBikesList.push(bike);
+					} else {
+						// if match found, update Price property and keep all other properties the same
+						const newBike = {
+							// ... spread operator keeps all properties the same, copies things as they were
+							...bike,
+							// changing the price here
+							price: newPrice,
+						};
+						newBikesList.push(newBike);
+					}
+				}
+
+				// change bikesList with setBikesList
+				// change state with new list
+				// NEED TO USE SET FUNCTION AND USE NEW ARRAY TO UPDATE STATE TO USE NEW LIST
+				setBikesList(newBikesList);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	// add new delete function
 	const deleteBike = (bikeId) => {
 		console.log("deleteBike Called");
-		const newBikesList = [];
-		for (const bike of bikesList) {
-			if (bike.id !== bikeId) {
-				newBikesList.push(bike);
-			}
-		}
-		setBikesList(newBikesList);
+
+		// API changes - add axios
+		axios
+			// mirror path from backend code (Flasky)
+			.delete(`${URL}/${bikeId}`)
+			.then(() => {
+				const newBikesList = [];
+				for (const bike of bikesList) {
+					if (bike.id !== bikeId) {
+						newBikesList.push(bike);
+					}
+				}
+				setBikesList(newBikesList);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	return (
